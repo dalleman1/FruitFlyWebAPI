904773d5c5b76e91d91c8be06cde1d65e30d3321
Morten Dalsgaard <monglade.dalsgaard@gmail.com>
2020-05-01 12:06:21 +0200

    add data

--- FruitFly.DataLayer/Configurations/ReferencepointConfiguration.cs
+++ FruitFly.DataLayer/Configurations/ReferencepointConfiguration.cs
@@ -15,23 +15,74 @@ namespace FruitFly.Repository.Configurations
         public void Configure(EntityTypeBuilder<Referencepoint> entity)
         {
             var rand = new Random();
-            //250 referencepoints for testing
-            for (int i = 1; i < 250; i++)
-            {
-                entity.HasData(
-                    new Referencepoint
-                    {
-                        ReferencepointId = i,
-                        Category = rand.Next(1, 10),
-                        RSSI1 = rand.Next(1, 100),
-                        RSSI2 = rand.Next(1, 100),
-                        RSSI3 = rand.Next(1, 100),
-                        X = rand.Next(1, 100),
-                        Y = rand.Next(1, 100),
-                        //HeatmapId = i,
-                    }
-                    );
-            }
+            ////250 referencepoints for testing
+            //for (int i = 1; i < 250; i++)
+            //{
+            //    entity.HasData(
+            //        new Referencepoint
+            //        {
+            //            ReferencepointId = i,
+            //            Category = rand.Next(1, 10),
+            //            RSSI1 = rand.Next(1, 100),
+            //            RSSI2 = rand.Next(1, 100),
+            //            RSSI3 = rand.Next(1, 100),
+            //            X = rand.Next(1, 100),
+            //            Y = rand.Next(1, 100),
+            //        }
+            //        );
+            //}
+
+            entity.HasData(
+                new Referencepoint
+                {
+                    ReferencepointId = 1,
+                    Category = rand.Next(1,15),
+                    RSSI1 = rand.Next(1, 100),
+                    RSSI2 = rand.Next(1, 100),
+                    RSSI3 = rand.Next(1, 100),
+                    X = 1,
+                    Y = 1
+                }
+                );
+
+                        entity.HasData(
+                new Referencepoint
+                {
+                    ReferencepointId = 2,
+                    Category = rand.Next(1,15),
+                    RSSI1 = rand.Next(1, 100),
+                    RSSI2 = rand.Next(1, 100),
+                    RSSI3 = rand.Next(1, 100),
+                    X = 1,
+                    Y = 2
+                }
+                );
+                        entity.HasData(
+                new Referencepoint
+                {
+                    ReferencepointId = 3,
+                    Category = rand.Next(1,15),
+                    RSSI1 = rand.Next(1, 100),
+                    RSSI2 = rand.Next(1, 100),
+                    RSSI3 = rand.Next(1, 100),
+                    X = 2,
+                    Y = 1
+                }
+                );
+
+                        entity.HasData(
+                new Referencepoint
+                {
+                    ReferencepointId = 4,
+                    Category = rand.Next(1,15),
+                    RSSI1 = rand.Next(1, 100),
+                    RSSI2 = rand.Next(1, 100),
+                    RSSI3 = rand.Next(1, 100),
+                    X = 2,
+                    Y = 2
+                }
+                );
+
         }
     }
 }
--- FruitFly.DataLayer/Migrations/20200501091939_initial.Designer.cs
+++ FruitFly.DataLayer/Migrations/20200501091939_initial.Designer.cs
@@ -4,14 +4,16 @@ using FruitFly.Repository.Configurations;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FruitFly.Repository.Migrations
 {
     [DbContext(typeof(FruitFlyContext))]
-    partial class FruitFlyContextModelSnapshot : ModelSnapshot
+    [Migration("20200501091939_initial")]
+    partial class initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
